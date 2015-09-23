32fbfdb5e1a195738715e7a2b6de6eaa5e62b076
mifl <mifl.pc@gmail.com>
2015-09-23 11:06:50 +0200

    ef63: merge configuration files
    Change-Id: I0126f1adf5aff10fdd0819e76750f923555d1de0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,36 +14,122 @@
 # limitations under the License.
 #
 
-# Inherit from MSM8974 common
--include device/pantech/msm8974-common/BoardConfigCommon.mk
+# inherit from pantech common
+#-include device/pantech/common/BoardConfigCommon.mk
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := device/pantech/ef63s/include
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := krait
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := ef63s,ef63k,ef63l,IM-A910S,IM-A910K,IM-A910L
 
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/pantech/ef63s/mkbootimg.mk
+BOARD_KERNEL_CMDLINE := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1000000 --tags_offset 0x00000100
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := ef63s_defconfig
+TARGET_KERNEL_SOURCE := kernel/pantech/ef63-common
+CM_DTS_TARGET := 910
+
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/pantech/ef63s/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+QCOM_BT_USE_SMD_TTY := true
+BLUETOOTH_HCI_USE_MCT := true
+
+# Board
+BOARD_VENDOR := pantech
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 COMMON_GLOBAL_CFLAGS += -DPANTECH_CAMERA_HARDWARE
 
+# Charger
+BOARD_CHARGER_RES := device/pantech/ef63s/charger/images
+
+# CM Hardware
+BOARD_HARDWARE_CLASS := device/pantech/ef63s/cmhw
+
+# Encryption
+#TARGET_HW_DISK_ENCRYPTION := true
+
 # Filesystem
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 16777216
 BOARD_CACHEIMAGE_PARTITION_SIZE    := 536870912
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_PARTITION_SIZE  := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1073741824
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 13747929088 # 13747945472 - 16384 for crypto footer
 #BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
+TARGET_USERIMAGES_USE_EXT4 := true
 
-# Kernel
-BOARD_KERNEL_CMDLINE := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0 androidboot.selinux=permissive
-TARGET_KERNEL_CONFIG := ef63s_defconfig
-TARGET_KERNEL_SOURCE := kernel/pantech/ef63-common
-CM_DTS_TARGET := 910
+# Flags
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Graphics
+BOARD_EGL_CFG := device/pantech/ef63s/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := false
+TARGET_USES_ION := true
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+HAVE_ADRENO_SOURCE:= false
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
 
 # NFC
 BOARD_NFC_CHIPSET := pn547
+
+# Protobuf: Added to indicate that protobuf-c is supported in this build
+PROTOBUF_SUPPORTED := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON       := true
@@ -68,8 +154,46 @@ PRODUCT_BUILD_PROP_OVERRIDES     += BUILD_UTC_DATE=0
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/pantech/ef63s/releasetools
 
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# QCOM Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# QCRIL
+TARGET_RIL_VARIANT := caf
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+#WITH_DEXPREOPT := true
+
+# SELinux policies
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+        device/pantech/ef63s/sepolicy
+
 # Vendor Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/pantech/ef63s/init/init_ef63s.c
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+TARGET_USES_WCNSS_CTRL := true
+TARGET_USES_QCOM_WCNSS_QMI := true
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/pantech/ef63s/BoardConfigVendor.mk
